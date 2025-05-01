41d627c4c1f48b8948dfb1b8aed7af66b354798f
pantaleone-ai <mdptrading@gmail.com>
2025-04-30 22:44:15 -0600

    add long_description

--- app/products/[slug]/details.tsx
+++ app/products/[slug]/details.tsx
@@ -40,6 +40,7 @@ interface Product {
   approved: boolean
   labels: string[]
   categories: string
+  long_description: string
 }
 
 export const ProductDetails = ({ product }: { product: Product }) => (
@@ -90,8 +91,11 @@ export const ProductDetails = ({ product }: { product: Product }) => (
               alt={`${product.full_name} image`}
             />
           </div>
-          <CardDescription className="text-2xl tracking-tight leading-tight text-neutral-800 text-balance dark:text-neutral-400">
+          <CardDescription className="text-2xl leading-tight text-neutral-800 text-balance dark:text-neutral-400">
             {product.description}
+          </CardDescription>
+          <CardDescription className="pt-8 text-xl tracking-tight leading-tight text-neutral-800 text-balance dark:text-neutral-400">
+            {product.long_description}
           </CardDescription>
 
           <div className="md:text-xl sm:text-lg tracking-tight text-neutral-800 text-balance dark:text-neutral-400 flex gap-2 items-center flex-wrap text-sm">
