23f0805414ec72fbe1daf0bee3d1f7b25768e196
pantaleone-ai <mdptrading@gmail.com>
2025-05-25 10:45:45 -0600

    cn fix announce banner test

--- components/announcement.jsx
+++ components/announcement.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
 
 const Announcement = () => {
   return (
-                    <div className="sticky shadow-sm top-0 border-b border-gray-800 bg-black px-4 py-1 text-gray-200 z-20">
+                    
+                    <div className={cn("sticky shadow-sm top-0 border-b border-gray-800 bg-black px-4 py-1 text-gray-200 z-20",
+      )}>
                   <p className="text-center font-medium text-sm/6">
                    mixPHD recommended
                     <a href="https://amzn.to/3SmSAHM" className="inline-block underline p-1" target="blank"> barwear deals @ Amazon</a>
